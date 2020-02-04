f06acaf83afc8c04565cc0c5a308e8c84e2c15ae
Marco <56915570+marcodmrn@users.noreply.github.com>
2020-02-04 18:40:55 +0100

    modified perso page

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,27 @@
-var menuMobile = document.querySelector(".headernavMobile");
-var iconMenu = document.querySelector(".headericonMenu");
-var iconClose = document.querySelector(".header__closeMenu");
+// var menuMobile = document.querySelector(".headernavMobile");
+// var iconMenu = document.querySelector(".headericonMenu");
+// var iconClose = document.querySelector(".header__closeMenu");
 
-iconMenu.addEventListener("click", function() {
-  menuMobile.classList.add("is-visible");
-});
+// iconMenu.addEventListener("click", function() {
+//   menuMobile.classList.add("is-visible");
+// });
 
-iconClose.addEventListener("click", function() {
-  menuMobile.classList.remove("is-visible");
-});
+// iconClose.addEventListener("click", function() {
+//   menuMobile.classList.remove("is-visible");
+// });
+
+// Personalisation
+
+let divChoices = document.querySelectorAll(".mainContent__choice");
+
+for (let i = 0; i < divChoices.length; i++) {
+  let divChoice = divChoices[i];
+  divChoice.addEventListener("click", function() {
+    let infosDiv = divChoice.querySelector(".mainContent__infos");
+    let titleDiv = divChoice.querySelector("h2");
+    let subtitleDiv = divChoice.querySelector("h3");
+    infosDiv.style.transform = "translateY(0)";
+    titleDiv.style.display = "none";
+    subtitleDiv.style.display = "block";
+  });
+}
