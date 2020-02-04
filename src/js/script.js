a439702e7788c3489eefbed250e654e74a35a36f
unknown <charles.cousin@hetic.net>
2020-02-04 11:38:26 +0100

    hovers

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,6 @@
 var menuMobile = document.querySelector(".headernavMobile");
 var iconMenu = document.querySelector(".headericonMenu");
-var iconClose = document.querySelector(".header__closeMenu");
 
 iconMenu.addEventListener("click", function() {
   menuMobile.classList.add("is-visible");
-});
-
-iconClose.addEventListener("click", function() {
-  menuMobile.classList.remove("is-visible");
 });
