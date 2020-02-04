4c221341dc22a5cfb6c5cc40c4259a42c400a852
Marco <56915570+marcodmrn@users.noreply.github.com>
2020-02-04 19:34:11 +0100

    resize

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
-// var menuMobile = document.querySelector(".headernavMobile");
-// var iconMenu = document.querySelector(".headericonMenu");
-// var iconClose = document.querySelector(".header__closeMenu");
+var menuMobile = document.querySelector(".headernavMobile");
+var iconMenu = document.querySelector(".headericonMenu");
+var iconClose = document.querySelector(".header__closeMenu");
 
-// iconMenu.addEventListener("click", function() {
-//   menuMobile.classList.add("is-visible");
-// });
+iconMenu.addEventListener("click", function() {
+  menuMobile.classList.add("is-visible");
+});
 
-// iconClose.addEventListener("click", function() {
-//   menuMobile.classList.remove("is-visible");
-// });
+iconClose.addEventListener("click", function() {
+  menuMobile.classList.remove("is-visible");
+});
 
 // Personalisation
 
